757c8e5d8dc1a011eb864eb1b865132f62aa065a
Laura Royle <lauraamie.royle@gmail.com>
2019-01-29 15:58:58 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,9 @@
-def prime?(number)
-  return false if number <= 1
-  Math.sqrt(number).to_i.downto(2).each {|i| return false if number % i == 0}
-  true
+#def prime?(number)
+#  return false if number <= 1
+#  Math.sqrt(number).to_i.downto(2).each {|i| return false if number % i == 0}
+#  true
+# end
+
+def prime?(n)
+  (2..n-1).none? {|divisor| n % divisor == 0}
 end
